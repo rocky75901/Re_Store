6977b395df1998ba720fc3c4b93d09102f128c02
Anirudh <anirudhsrivatsa820@gmail.com>
2025-03-03 22:05:22 +0530

    Changed home to layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,22 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Login from "./pages/auth/Login";
-import Home from "./pages/auth/home";
 import SignUp from "./pages/auth/Signup";
 import ForgotPassword from "./pages/auth/ForgotPassword";
 import Verify from "./pages/auth/verify";
+import Layout from "./pages/auth/layout";
 
 const App = () => (
   <Router>
     <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/home" element={<Home />} />      
+      <Route path="/" element={<Login />} />     
       <Route path="/login" element={<Login />} />
       <Route path="/sign-up" element={<SignUp />} />
       <Route path="/forgot-password" element={<ForgotPassword />} />
       <Route path="/verify" element={<Verify />} />
+      <Route path="/layout" element={<Layout />} />
+        {/* <Route path="/home" element={<Home />} />  */}
+      
     </Routes>
   </Router>
 );
