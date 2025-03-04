c6719e7b5d6ebe9f201d0c7a7ae7fe34d71a4d59
Your Name <yerramachulakshmi@gmail.com>
2025-03-04 22:15:44 +0530

    re pushing verify page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,9 @@ import Login from "./pages/auth/Login";
 import SignUp from "./pages/auth/Signup";
 import ForgotPassword from "./pages/auth/ForgotPassword";
 import Verify from "./pages/auth/verify";
-<<<<<<< HEAD
 import ResetPassword from "./pages/auth/resetpassword";
-=======
+
 import Layout from "./pages/auth/layout";
->>>>>>> 6977b395df1998ba720fc3c4b93d09102f128c02
 
 const App = () => (
   <Router>
@@ -18,13 +16,8 @@ const App = () => (
       <Route path="/sign-up" element={<SignUp />} />
       <Route path="/forgot-password" element={<ForgotPassword />} />
       <Route path="/verify" element={<Verify />} />
-<<<<<<< HEAD
       <Route path="/resetpassword" element={<ResetPassword />} />
-=======
       <Route path="/layout" element={<Layout />} />
-        {/* <Route path="/home" element={<Home />} />  */}
-      
->>>>>>> 6977b395df1998ba720fc3c4b93d09102f128c02
     </Routes>
   </Router>
 );
